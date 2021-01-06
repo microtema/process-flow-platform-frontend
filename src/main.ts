3ab64ecbbb8277f0626d0b3ed689f788068df061
mtema <mtema@it-economics.de>
2021-01-06 21:56:54 +0100

    DX-1000 implement fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 import App from './app/App.vue';
 import './registerServiceWorker';
 import router from './router';
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,7 @@
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
+  import { defineComponent } from 'vue';
 
-  const component: DefineComponent<{}, {}, never>;
+  const component: ReturnType<typeof defineComponent>;
+
   export default component;
 }
